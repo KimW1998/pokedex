a08b00b47a0ba236d5f599ce21c74ca6e4b17e22
KimW1998 <72084242+KimW1998@users.noreply.github.com>
2022-06-23 10:15:33 +0200

    created a simple navbar with links to basic pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import "./App.css";
+import { Routes, Route, NavLink } from "react-router-dom";
+import HomePage from "./pages.tsx/HomePage";
+import AboutPage from "./pages.tsx/AboutPage";
+import DiscoverPage from "./pages.tsx/DiscoverPage";
 
 function App() {
   return (
     <div className="App">
-      <h1>test</h1>
+      <div className="Navbar">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">AboutPage</NavLink>
+        <NavLink to="/discover">DiscoverPage</NavLink>
+      </div>
+      <Routes>
+        <Route path="/discover" element={<DiscoverPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages.tsx/DiscoverPage.tsx
@@ -0,0 +1,5 @@
+const DiscoverPage = () => {
+  return <h1>DiscoverPage</h1>;
+};
+
+export default DiscoverPage;
