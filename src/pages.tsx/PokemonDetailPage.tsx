48db3e73e14256e4af1aaa2aab91f2f29f0d7eb9
KimW1998 <72084242+KimW1998@users.noreply.github.com>
2022-06-23 21:40:37 +0200

    fixed connection error

--- src/pages.tsx/PokemonDetailPage.tsx
+++ src/pages.tsx/PokemonDetailPage.tsx
@@ -37,7 +37,7 @@ const PokemonDetailPage = () => {
     };
 
     FetchData();
-  }, [routeParams.pokemon_name]);
+  }, []);
 
   return pokemonsDetails ? (
     <div className="pokemon-detail-container ">
